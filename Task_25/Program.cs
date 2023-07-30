08d608b35dcb8c060ad1e90bcb21dbc0c5356dc6
eN1nja <mr.buntar@gmail.com>
2023-07-30 22:16:09 +0300

    занес задачи в git

---
+++ Task_25/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+Console.Write("Введите число: ");
+int numberA;
+bool isNumberAParsed = int.TryParse(Console.ReadLine(), out numberA);
+
+Console.Write("Введите степень: ");
+int numberB;
+bool isNumberBParsed = int.TryParse(Console.ReadLine(), out numberB);
+
+if (isNumberAParsed && isNumberBParsed)
+{
+    void ToDegree(int a, int b)
+    {
+        int result = 1;
+        for (int i = 1; i <= b; i++)
+        {
+            result *= a;
+        }
+        Console.WriteLine(a + " в степени " + b + " = " + result);
+    }
+
+    ToDegree(numberA, numberB);
+}
+else
+{
+    Console.WriteLine("Введите числа!");
+}
---
+++ Task_27/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите число: ");
+        int number;
+        bool isNumberParsed = int.TryParse(Console.ReadLine(), out number);
+        int sumOfDigits = 0;
+
+        if (isNumberParsed)
+        {           
+            while (number > 0)
+            {
+                int digit = number % 10;
+                sumOfDigits += digit;
+                number /= 10;
+            }
+
+            Console.WriteLine("Сумма цифр в числе: " + sumOfDigits);
+        }
+        else
+        {
+            Console.WriteLine("Введите цифры!");
+        }
+    }
+}
